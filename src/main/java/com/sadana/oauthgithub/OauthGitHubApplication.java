03adc4fc105e0baf5aa9323c2a4d55cad9dab757
Sadana-Tech <Kriday06@gmail.com>
2023-10-19 05:30:33 +0530

    Initial Commit

---
+++ src/main/java/com/sadana/oauthgithub/OauthGitHubApplication.java
@@ -0,0 +1,13 @@
+package com.sadana.oauthgithub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OauthGitHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OauthGitHubApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sadana/oauthgithub/OauthGitHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sadana.oauthgithub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OauthGitHubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
